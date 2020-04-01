95eba92c7db683f27ebfddf1dd75f3b558919721
Nevyn Bengtsson <nevyn.jpg@gmail.com>
2020-04-01 22:12:30 +0200

    Stub for pico headset module

---
+++ src/modules/headset/pico.c
@@ -0,0 +1,11 @@
+#include "headset/headset.h"
+#include "event/event.h"
+#include "graphics/graphics.h"
+#include "graphics/canvas.h"
+#include "core/maf.h"
+#include "core/os.h"
+#include "core/util.h"
+#include <math.h>
+#include <stdint.h>
+#include <stdbool.h>
+
