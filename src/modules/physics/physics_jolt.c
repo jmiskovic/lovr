4e18351022eb95aa7c1756c95d04541af42db417
Josip Miskovic <josipmiskovic@gmail.com>
2023-09-04 23:12:50 +0200

    Replace jolt C bindings; back to stubs

--- src/modules/physics/physics_jolt.c
+++ src/modules/physics/physics_jolt.c
@@ -1,209 +1,18 @@
-#include <stdlib.h>
-#include "util.h"
 #include "physics.h"
-#include "JoltPhysicsC.h"
+#include "util.h"
+#include <stdlib.h>
 
-static JPC_TempAllocator *temp_allocator;
-static JPC_JobSystem *job_system;
-JPC_BodyInterface * body_interface; // todo
-
-struct World {
-  JPC_PhysicsSystem *physics_system;
-  JPC_BodyInterface *body_interface;
-};
-
-struct Collider {
-  JPC_BodyID id;
-  JPC_Body *body;
-};
-
-struct Shape {
-};
-
-// Object layers
-#define NUM_OBJ_LAYERS 2
-#define OBJ_LAYER_NON_MOVING 0
-#define OBJ_LAYER_MOVING 1
-
-// Broad phase layers
-#define NUM_BP_LAYERS 2
-#define BP_LAYER_NON_MOVING 0
-#define BP_LAYER_MOVING 1
-
-typedef struct BPLayerInterfaceImpl
-{
-    const JPC_BroadPhaseLayerInterfaceVTable *vtable; // VTable has to be the first field in the struct.
-    JPC_BroadPhaseLayer                       object_to_broad_phase[NUM_OBJ_LAYERS];
-} BPLayerInterfaceImpl;
-
-
-static uint32_t
-BPLayerInterface_GetNumBroadPhaseLayers(const void *in_self)
-{
-    return NUM_BP_LAYERS;
-}
-
-
-static JPC_BroadPhaseLayer
-BPLayerInterface_GetBroadPhaseLayer(const void *in_self, JPC_ObjectLayer in_layer)
-{
-  lovrAssert(in_layer < NUM_BP_LAYERS, "Broad-phase layer out of range");
-  const BPLayerInterfaceImpl *self = (BPLayerInterfaceImpl *)in_self;
-  return self->object_to_broad_phase[in_layer];
-}
-
-
-static BPLayerInterfaceImpl
-BPLayerInterface_Create(void)
-{
-  static const JPC_BroadPhaseLayerInterfaceVTable vtable =
-  {
-    .GetNumBroadPhaseLayers = BPLayerInterface_GetNumBroadPhaseLayers,
-    .GetBroadPhaseLayer   = BPLayerInterface_GetBroadPhaseLayer,
-  };
-  BPLayerInterfaceImpl impl =
-  {
-    .vtable = &vtable,
-  };
-  impl.object_to_broad_phase[OBJ_LAYER_NON_MOVING] = BP_LAYER_NON_MOVING;
-  impl.object_to_broad_phase[OBJ_LAYER_MOVING]   = BP_LAYER_MOVING;
-
-  return impl;
-}
-
-
-
-typedef struct MyObjectFilter
-{
-    const JPC_ObjectLayerPairFilterVTable *vtable; // VTable has to be the first field in the struct.
-} MyObjectFilter;
-
-
-static bool
-MyObjectFilter_ShouldCollide(const void *in_self, JPC_ObjectLayer in_object1, JPC_ObjectLayer in_object2)
-{
-    switch (in_object1)
-    {
-        case OBJ_LAYER_NON_MOVING:
-            return in_object2 == OBJ_LAYER_MOVING;
-        case OBJ_LAYER_MOVING:
-            return true;
-        default:
-            lovrAssert(false, "Collision check between unknown layers");
-            return false;
-    }
-}
-
-static MyObjectFilter
-MyObjectFilter_Create(void)
-{
-    static const JPC_ObjectLayerPairFilterVTable vtable =
-    {
-        .ShouldCollide = MyObjectFilter_ShouldCollide,
-    };
-    MyObjectFilter impl =
-    {
-        .vtable = &vtable,
-    };
-    return impl;
-}
-
-
-
-typedef struct MyBroadPhaseFilter
-{
-    const JPC_ObjectVsBroadPhaseLayerFilterVTable *vtable; // VTable has to be the first field in the struct.
-} MyBroadPhaseFilter;
-
-static bool
-MyBroadPhaseFilter_ShouldCollide(const void *in_self, JPC_ObjectLayer in_layer1, JPC_BroadPhaseLayer in_layer2)
-{
-    switch (in_layer1)
-    {
-        case OBJ_LAYER_NON_MOVING:
-            return in_layer2 == BP_LAYER_MOVING;
-        case OBJ_LAYER_MOVING:
-            return true;
-        default:
-            lovrAssert(false, "Broad-phase collision check between unknown layers");
-            return false;
-    }
-}
-
-
-static MyBroadPhaseFilter
-MyBroadPhaseFilter_Create(void)
-{
-    static const JPC_ObjectVsBroadPhaseLayerFilterVTable vtable =
-    {
-        .ShouldCollide = MyBroadPhaseFilter_ShouldCollide,
-    };
-    MyBroadPhaseFilter impl =
-    {
-        .vtable = &vtable,
-    };
-    return impl;
-}
-
-
-
-
-bool lovrPhysicsInit(void) {
-  JPC_RegisterDefaultAllocator();
-  JPC_CreateFactory();
-  JPC_RegisterTypes();
-  temp_allocator = JPC_TempAllocator_Create(32 * 1024 * 1024);
-  job_system = JPC_JobSystem_Create(JPC_MAX_PHYSICS_JOBS, JPC_MAX_PHYSICS_BARRIERS, -1);
-}
+bool lovrPhysicsInit(void) {}
 
 void lovrPhysicsDestroy(void) {}
 
-World* lovrWorldCreate(float xg, float yg, float zg, bool allowSleep, const char** tags, uint32_t tagCount) {
-  World* world = calloc(1, sizeof(World));
-  lovrAssert(world, "Out of memory");
-  const uint32_t max_bodies = 10240;
-  const uint32_t num_body_mutexes = 0; // zero is auto-detect
-  const uint32_t max_body_pairs = 65536;
-  const uint32_t max_contact_constraints = 20480;
-
-  BPLayerInterfaceImpl *broad_phase_layer_interface = malloc(sizeof(BPLayerInterfaceImpl));
-  *broad_phase_layer_interface = BPLayerInterface_Create();
-
-  MyBroadPhaseFilter *broad_phase_filter = malloc(sizeof(MyBroadPhaseFilter));
-  *broad_phase_filter = MyBroadPhaseFilter_Create();
-
-  MyObjectFilter *object_filter = malloc(sizeof(MyObjectFilter));
-  *object_filter = MyObjectFilter_Create();
-
-  world->physics_system = JPC_PhysicsSystem_Create(
-    max_bodies,
-    num_body_mutexes,
-    max_body_pairs,
-    max_contact_constraints,
-    broad_phase_layer_interface,
-    broad_phase_filter,
-    object_filter);
-  world->body_interface = JPC_PhysicsSystem_GetBodyInterface(world->physics_system);
-  body_interface = world->body_interface;
-  float const gravity[3] = {0.f, 0.f, 0.f};
-  JPC_PhysicsSystem_SetGravity(world->physics_system , gravity);
-
-  return world;
-}
+World* lovrWorldCreate(float xg, float yg, float zg, bool allowSleep, const char** tags, uint32_t tagCount) {}
 
 void lovrWorldDestroy(void* ref) {}
 
 void lovrWorldDestroyData(World* world) {}
 
-void lovrWorldUpdate(World* world, float dt, CollisionResolver resolver, void* userdata) {
-  JPC_PhysicsUpdateError update_err = JPC_PhysicsSystem_Update(
-    world->physics_system,
-    dt,
-    1,
-    1,
-    temp_allocator,
-    job_system);
-}
+void lovrWorldUpdate(World* world, float dt, CollisionResolver resolver, void* userdata) {}
 
 int lovrWorldGetStepCount(World* world) {}
 
@@ -257,35 +66,7 @@ void lovrWorldEnableCollisionBetween(World* world, const char* tag1, const char*
 
 bool lovrWorldIsCollisionEnabledBetween(World* world, const char* tag1, const char* tag2) {}
 
-Collider* lovrColliderCreate(World* world, float x, float y, float z) {
-  Collider* collider = calloc(1, sizeof(Collider));
-  lovrAssert(collider, "Out of memory");
-  float w = .2f;
-  float h = .2f;
-  float d = .2f;
-  JPC_BoxShapeSettings *box_shape_settings = JPC_BoxShapeSettings_Create((float[]){ w, h, d });
-  JPC_Shape *box_shape = JPC_ShapeSettings_CreateShape((JPC_ShapeSettings *)box_shape_settings);
-
-  JPC_BodyCreationSettings box_settings;
-  JPC_BodyCreationSettings_Set(
-      &box_settings,
-      box_shape,
-      (JPC_Real[]){ x, y, z },
-      (float[]){ 0.0f, 0.0f, 0.0f, 1.0f },
-      JPC_MOTION_TYPE_DYNAMIC,
-      OBJ_LAYER_MOVING);
-
-  JPC_Body *box = JPC_BodyInterface_CreateBody(world->body_interface, &box_settings);
-  const JPC_BodyID box_id = JPC_Body_GetID(box);
-  JPC_BodyInterface_AddBody(world->body_interface, box_id, JPC_ACTIVATION_ACTIVATE);
-  collider->id = box_id;
-  collider->body = box;
-
-  JPC_PhysicsSystem_OptimizeBroadPhase(world->physics_system); // todo!
-  const float force[3] = {1.f, 1.f, 0.f};
-  JPC_Body_AddForce(box, force);
-  return collider;
-}
+Collider* lovrColliderCreate(World* world, float x, float y, float z) {}
 
 void lovrColliderDestroy(void* ref) {}
 
@@ -348,20 +129,11 @@ void lovrColliderGetMassData(Collider* collider, float* cx, float* cy, float* cz
 
 void lovrColliderSetMassData(Collider* collider, float cx, float cy, float cz, float mass, float inertia[6]) {}
 
-void lovrColliderGetPosition(Collider* collider, float* x, float* y, float* z)
-{
-  JPC_Real position[3];
-  JPC_Body_GetPosition(collider->body, &position[0]);
-  *x = position[0];
-  *y = position[1];
-  *z = position[2];
-}
+void lovrColliderGetPosition(Collider* collider, float* x, float* y, float* z) {}
 
 void lovrColliderSetPosition(Collider* collider, float x, float y, float z) {}
 
-void lovrColliderGetOrientation(Collider* collider, float* orientation) {
-  JPC_BodyInterface_GetRotation(body_interface, collider->id, orientation);
-}
+void lovrColliderGetOrientation(Collider* collider, float* orientation) {}
 
 void lovrColliderSetOrientation(Collider* collider, float* orientation) {}
 
@@ -442,12 +214,7 @@ float lovrSphereShapeGetRadius(SphereShape* sphere) {}
 
 void lovrSphereShapeSetRadius(SphereShape* sphere, float radius) {}
 
-BoxShape* lovrBoxShapeCreate(float w, float h, float d) {
-  BoxShape* box = calloc(1, sizeof(BoxShape));
-  return box;
-}
-
-
+BoxShape* lovrBoxShapeCreate(float w, float h, float d) {}
 
 void lovrBoxShapeGetDimensions(BoxShape* box, float* w, float* h, float* d) {}
 
